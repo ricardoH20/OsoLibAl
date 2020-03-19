902113319e9d2093d8f1bcb5f1a5100765c932ae
ricardoH20 <60334119+ricardoH20@users.noreply.github.com>
2020-03-18 19:34:22 -0600

    Terminando OVecr3

--- src/hn/edu/ujcv/progra/OVecR3.java
+++ src/hn/edu/ujcv/progra/OVecR3.java
@@ -44,12 +44,7 @@ public class OVecR3 {
     }
 
     public OVecR3 prodCruz(OVecR3 b){
-        //
-        //  x  y  z
-        //  x1 y1 z1       i = y * z1 - z * y1;
-        //                 j = - ();
-        //                 k = ;
-        //
+
         return new OVecR3();
     }
 
@@ -63,8 +58,59 @@ public class OVecR3 {
         return 0.0f;
     }
 
+
     // metodos
+    public OVecR3 suma(OVecR3 b)
+    {
+
+        return new OVecR3(this.x + b.x, this.v + b.v, this.z + b.z);
+    }
+
+    public OVecR3 resta(OVecR3 b)
+    {
+        return new OVecR3(this.x - b.x, this.v - b.v, this.z - b.z);
+    }
+
+    public OVecR3 prodCruz(OVecR3 b)
+    {
+        double i, j, k;
+        i = (this.v * b.z) - (this.z * b.v);
+        j = -((this.x * b.z) - (this.z * b.x));
+        k =  (this.x * b.v) - (this.v * b.x);
+        return new OVecR3(i,j,k);
+    }
+
+    public double prodPunto(OVecR3 b){
+        double respuesta;
+        respuesta = (this.x * b.x) + (this.y * b.y) + (this.z * b.z);
+        return respuesta;
+    }
+
+    public double magnitud()
+    {
+        double respuesta;
+        respuesta = Math.sqrt( Math.pow(this.x,2) + Math.pow(this.y,2) + Math.pow(this.z,2));
+        return respuesta;
+    }
+
+    @java.lang.Override
+    public String toString(){
+        return "i: " + getX() + " j: " + getY() + " k: " + getZ();
+    }
+
+
+
 
 
     // constructores
+    public OVecR3(){}
+
+    public OVecR3(OVecR3 a){}
+
+    public OVecR3(double x, double y,double z)
+    {
+        this.x = x;
+        this.v = v;
+        this.z = z;
+    }
 }
