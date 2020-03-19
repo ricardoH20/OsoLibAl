390da7ab265cfb355516e101a21e54b14bc106ac
ricardoH20 <60334119+ricardoH20@users.noreply.github.com>
2020-03-18 19:35:28 -0600

    Terminando OVecr3

--- src/hn/edu/ujcv/progra/OVecR3.java
+++ src/hn/edu/ujcv/progra/OVecR3.java
@@ -32,33 +32,7 @@ public class OVecR3 {
     public double getV() {
         return v;
     }
-
-    public OVecR3 suma(OVecR3 b){
-        //TODO: implementar
-        return new OVecR3();
-    }
-
-    public OVecR3 resta(OVecR3 b){
-        //TODO: implementar
-        return new OVecR3();
-    }
-
-    public OVecR3 prodCruz(OVecR3 b){
-
-        return new OVecR3();
-    }
-
-    public double prodPunto(OVecR3 b){
-        //TODO: implementar
-        return 0.0f;
-    }
-
-    public double magnitud(){
-        //TODO: implementar
-        return 0.0f;
-    }
-
-
+    
     // metodos
     public OVecR3 suma(OVecR3 b)
     {
