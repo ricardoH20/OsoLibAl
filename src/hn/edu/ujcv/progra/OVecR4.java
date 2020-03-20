becf001d6630e6a67c137c7cee5dd10aa6d5bb4b
ricardoH20 <60334119+ricardoH20@users.noreply.github.com>
2020-03-19 23:20:51 -0600

    Retoque final de Ovecr4

--- src/hn/edu/ujcv/progra/OVecR4.java
+++ src/hn/edu/ujcv/progra/OVecR4.java
@@ -41,27 +41,31 @@ public class OVecR4 {
         this.z = z;
     }
     // metodos
-
-    public OVecR4 suma(OVecR4 b){
-        //TODO: implementar
-        return new OVecR4();
+    public OVecR4 suma(OVecR4 b) {
+        return new OVecR4(this.x + b.x, this.v + b.v, this.z + b.z, this.v + b.v);
     }
 
-    public OVecR4 resta(OVecR4 b){
-        //TODO: implementar
-        return new OVecR4();
+    public OVecR4 resta(OVecR4 b) {
+        return new OVecR4(this.x - b.x, this.v - b.v, this.z - b.z, this.w - b.w);
     }
 
-    public double prodPunto(OVecR4 b){
-        //TODO: implementar
-        return 0.0f;
+    public double prodPunto(OVecR4 b) {
+        double respuesta;
+        respuesta = (this.x * b.x) + (this.v * b.v) + (this.z * b.z) + (this.w * b.w);
+        return respuesta;
     }
 
-    public double magnitud(){
-        //TODO: implementar
-        return 0.0f;
+    public double magnitud()
+    {
+        double respuesta;
+        respuesta = Math.sqrt( Math.pow(this.x,2) + Math.pow(this.v,2) + Math.pow(this.z,2) + Math.pow(this.v,2));
+        return respuesta;
     }
 
+    @Override
+    public String toString(){
+        return "i: " + getX() + " j: " + getV() + " k: " + getZ() + " t: " + getW();
+    }
     // constructores
     public OVecR4(){ }
     public OVecR4(OVecR4 a) {}
