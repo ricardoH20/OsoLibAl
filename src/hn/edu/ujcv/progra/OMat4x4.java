b6514e2b86b769a73285a855fd8cf284aaabc9e9
ricardoH20 <60334119+ricardoH20@users.noreply.github.com>
2020-03-19 18:16:08 -0600

    Añadiendo seters y geters

--- src/hn/edu/ujcv/progra/OMat4x4.java
+++ src/hn/edu/ujcv/progra/OMat4x4.java
@@ -6,11 +6,138 @@ public class OMat4x4 {
     private double m21,m22,m23,m24;
     private double m31,m32,m33,m34;
     private double m41,m42,m43,m44;
-    
+
 
 
     // accesoras y mutadoras
 
+    public double getM11() {
+        return m11;
+    }
+
+    public double getM12() {
+        return m12;
+    }
+
+    public double getM21() {
+        return m21;
+    }
+
+    public double getM22() {
+        return m22;
+    }
+
+    public void setM11(double m11) {
+        this.m11 = m11;
+    }
+
+    public void setM12(double m12) {
+        this.m12 = m12;
+    }
+
+    public void setM21(double m21) {
+        this.m21 = m21;
+    }
+
+    public void setM22(double m22) {
+        this.m22 = m22;
+    }
+
+    public void setM33(double m33) {
+        this.m33 = m33;
+    }
+
+    public void setM32(double m32) {
+        this.m32 = m32;
+    }
+
+    public void setM31(double m31) {
+        this.m31 = m31;
+    }
+
+    public void setM23(double m23) {
+        this.m23 = m23;
+    }
+
+    public void setM13(double m13) {
+        this.m13 = m13;
+    }
+
+    public double getM33() {
+        return m33;
+    }
+
+    public double getM32() {
+        return m32;
+    }
+
+    public double getM31() {
+        return m31;
+    }
+
+    public double getM23() {
+        return m23;
+    }
+
+    public double getM13() {
+        return m13;
+    }
+
+    public double getM14() {
+        return m14;
+    }
+
+    public double getM24() {
+        return m24;
+    }
+
+    public double getM34() {
+        return m34;
+    }
+
+    public double getM41() {
+        return m41;
+    }
+
+    public double getM42() {
+        return m42;
+    }
+
+    public double getM43() {
+        return m43;
+    }
+
+    public double getM44() {
+        return m44;
+    }
+
+    public void setM14(double m14) {
+        this.m14 = m14;
+    }
+
+    public void setM24(double m24) {
+        this.m24 = m24;
+    }
+
+    public void setM34(double m34) {
+        this.m34 = m34;
+    }
+
+    public void setM41(double m41) {
+        this.m41 = m41;
+    }
+
+    public void setM42(double m42) {
+        this.m42 = m42;
+    }
+
+    public void setM43(double m43) {
+        this.m43 = m43;
+    }
+
+    public void setM44(double m44) {
+        this.m44 = m44;
+    }
     // metodos
 
     public OMat4x4 transpuesta(){
